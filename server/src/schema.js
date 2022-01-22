c5848f8d0c154224f702aa70cf2529f0dc54c36f
Jerry Ng <ngshiheng@gmail.com>
2022-01-22 10:15:44 +0800

    feat: update schema

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,26 +1,17 @@
 const { gql } = require('apollo-server-cloudflare')
 
 module.exports = gql`
-    type PokemonSprites {
-        front_default: String!
-        front_shiny: String!
-        front_female: String!
-        front_shiny_female: String!
-        back_default: String!
-        back_shiny: String!
-        back_female: String!
-        back_shiny_female: String!
-    }
-
-    type Pokemon {
-        id: ID!
-        name: String!
-        height: Int!
-        weight: Int!
-        sprites: PokemonSprites!
+    type Paste {
+        uuid: ID!
+        content: String!
+        url: String!
     }
 
     type Query {
-        pokemon(id: ID!): Pokemon
+        getPaste(uuid: ID!): Paste!
+    }
+
+    type Mutation {
+        createPaste(content: String!): Paste!
     }
 `
