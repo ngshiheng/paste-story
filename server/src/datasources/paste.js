4cf751a8afce636fce5c40dba94771789c410c99
Jerry Ng <ngshiheng@gmail.com>
2022-01-22 16:48:30 +0800

    feat: add createPaste

--- server/src/datasources/paste.js
+++ server/src/datasources/paste.js
@@ -1,5 +1,10 @@
 const { ApolloError } = require('apollo-server-cloudflare')
 class PasteAPI {
+    /*
+    Get a paste by its UUID (urlKey) from `PASTE_DB`.
+
+    Throws an error if paste is not found.
+    */
     async getPaste(uuid) {
         const paste = await PASTE_DB.get(uuid)
         if (!paste) {
@@ -8,16 +13,32 @@ class PasteAPI {
 
         return {
             uuid,
-            content,
+            content: paste,
             url: `https://paste.jerrynsh.com/${uuid}`,
         }
     }
 
+    /*
+    Create a new paste in `PASTE_DB`.
+    
+    Fetch a new UUID (urlKey) from `KEY_DB`.
+
+    UUID is then removed from `KEY_DB` to avoid duplicates.
+    */
     async createPaste(content) {
-        return {
-            uuid,
-            content: 'Hello world',
-            url: 'https://example.com',
+        try {
+            const uuid = await KEY_DB.list({ limit: 1 })
+
+            await KEY_DB.delete(uuid)
+            await PASTE_DB.put(uuid, content, { expirationTtl: 60 * 60 }) // TODO: make expirationTtl based on user input.
+
+            return {
+                uuid,
+                content,
+                url: `https://paste.jerrynsh.com/${uuid}`,
+            }
+        } catch (error) {
+            throw new ApolloError('Failed to create paste')
         }
     }
 }
